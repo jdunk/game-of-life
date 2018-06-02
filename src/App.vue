2a3251f41ce86c85479cfd0351337ed42c137214
jdunk <jdunk@users.noreply.github.com>
2018-06-02 17:39:41 +0800

    Initial commit: vuetify, eslint-airbnb, App and "GameView" component

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app
+    >
+      <v-toolbar-side-icon @click.stop=""></v-toolbar-side-icon>
+      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-content>
+      <GameView/>
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>by <a href="https://jaredduncan.com/" target="_blank">Jared Duncan</a></span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import GameView from './components/GameView';
+
+export default {
+  data() {
+    return {
+      title: 'Conway\'s Game of Life',
+    };
+  },
+  name: 'App',
+  components: {
+    GameView,
+  },
+};
+</script>
